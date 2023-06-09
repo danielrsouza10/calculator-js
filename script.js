35644f337d0facca574c27724f0622cdf26879f1
Daniel Souza <danielrsouza10@gmail.com>
2023-06-09 15:40:21 -0300

    made some improvements

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
-const numButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const buttons = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "+",
+  "-",
+  "*",
+  "/",
+  "=",
+  "Clear",
+];
 
-const operatorButtons = ["+", "-", "*", "/", "=", "C"];
 const container = document.querySelector(".container");
-let value = "";
+let value1 = "";
 let value2 = "";
 let operator = "";
 let result = 0;
+
+function numButton(value) {
+  if (value1 === "" || operator === "") {
+    value1 += value;
+  } else {
+    value2 += value;
+  }
+  console.log(value1);
+  console.log(value2);
+  console.log(operator);
+}
+
+function operatorButton(value) {
+  operator = value;
+}
+
+function operate() {
+  if (operator === "+") {
+    result = parseInt(value1) + parseInt(value2);
+    value1 = "";
+    value2 = "";
+    operator = "";
+    console.log(result);
+  } else if (operator === "-") {
+    result = parseInt(value1) - parseInt(value2);
+    value1 = "";
+    value2 = "";
+    operator = "";
+    console.log(result);
+  } else if (operator === "*") {
+    result = parseInt(value1) * parseInt(value2);
+    value1 = "";
+    value2 = "";
+    operator = "";
+    console.log(result);
+  } else if (operator === "/") {
+    result = parseInt(value1) / parseInt(value2);
+    value1 = "";
+    value2 = "";
+    operator = "";
+    console.log(result);
+  }
+}
 
 // function calculator(value1, value2, operator){
 //     let value1 = value1;
@@ -13,35 +72,44 @@ let result = 0;
 //     let operator = operator;
 // }
 
-function calculatorButtonsDisplay() {
-  numButtons.forEach((item) => {
-    const numButton = document.createElement("button");
-    numButton.textContent = item;
-    numButton.addEventListener("click", () => {
-      value = value + item;
+// function operate(value1, value2, operator) {
+//   if (operator === "+") {
+//     result = value1 + value2;
+//   } else if (operator === "-") {
+//     result = value1 - value2;
+//   } else if (operator === "/") {
+//     result = value1 / value2;
+//   } else if (operator === "*") {
+//     result = value1 * value2;
+//   }
+//   console.log(result);
+// }
 
-      // value.push(event.target.innerText);
-      console.log(value);
-    });
-    container.appendChild(numButton);
-  });
-  
-  operatorButtonsDisplay();
-}
+// function calculatorButtonsDisplay() {
+//   buttons.forEach((item) => {
+//     const button = document.createElement("button");
+//     button.textContent = item;
+//     container.appendChild(button);
+//   });
+// }
 
-function operatorButtonsDisplay() {
-  operatorButtons.forEach((item2) => {
-    const operatorButton = document.createElement("button");
-    operatorButton.textContent = item2;
-    operatorButton.addEventListener("click", () => {
-      operator = item2;
+// calculatorButtonsDisplay();
 
-      // value.push(event.target.innerText);
-      console.log(operator);
-    });
-    container.appendChild(operatorButton);
-  });
-}
-
-calculatorButtonsDisplay();
-
+// const button = document.querySelectorAll('button');
+// button.forEach((item) => addEventListener("click", () => {
+//   if (item >= 0 && item <= 9) {
+//     value1 = value1 + item;
+//     console.log(value1);
+//   } else {
+//     operator = item;
+//     console.log(operator);
+//     // button.addEventListener("click", () => {
+//     //   if (item >= 0 && item <= 9) {
+//     //     value2 = value2 + item;
+//     //   } else if (item === "=") {
+//     //     operate(value1, value2, operator);
+//     //   }
+//     // });
+//   }
+// })
+// );
