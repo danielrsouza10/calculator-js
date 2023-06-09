65ee004b76be3c7b698d88a6992695d06ade213d
Daniel Souza <danielrsouza10@gmail.com>
2023-06-09 10:37:10 -0300

    some ajustments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const buttons = ["0", "1", "2", 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "=", "C"];
+const numButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const operatorButtons = ["+", "-", "*", "/", "=", "C"];
 const container = document.querySelector(".container");
 let value = "";
 let value2 = "";
 let operator = "";
+let result = 0;
 
 // function calculator(value1, value2, operator){
 //     let value1 = value1;
@@ -10,22 +13,35 @@ let operator = "";
 //     let operator = operator;
 // }
 
-function calculatorButtons() {
-  buttons.forEach((item) => {
-    const button = document.createElement("button");
-    button.textContent = item;
-    button.addEventListener("click", (event) => {
-      if(item >= 0){
-        value = value + item;
-      }
+function calculatorButtonsDisplay() {
+  numButtons.forEach((item) => {
+    const numButton = document.createElement("button");
+    numButton.textContent = item;
+    numButton.addEventListener("click", () => {
+      value = value + item;
+
       // value.push(event.target.innerText);
       console.log(value);
     });
+    container.appendChild(numButton);
+  });
+  
+  operatorButtonsDisplay();
+}
 
-    container.appendChild(button);
+function operatorButtonsDisplay() {
+  operatorButtons.forEach((item2) => {
+    const operatorButton = document.createElement("button");
+    operatorButton.textContent = item2;
+    operatorButton.addEventListener("click", () => {
+      operator = item2;
+
+      // value.push(event.target.innerText);
+      console.log(operator);
+    });
+    container.appendChild(operatorButton);
   });
 }
 
-calculatorButtons();
-
+calculatorButtonsDisplay();
 
